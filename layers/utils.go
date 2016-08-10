5107b29d72f0b694ef26b009f07d05d2ee712aa3
Rompei <rompei0924@gmail.com>
2016-08-10 14:28:59 +0900

    Optimize im2col.

--- layers/utils.go
+++ layers/utils.go
@@ -7,23 +7,30 @@ func Im2Col(img [][][]float32, kernelSize, stride int) [][]float32 {
 	cols := (len(img[0][0])-kernelSize)/stride + 1
 	res := make([][]float32, rows*cols)
 	idx1 := 0
+	doneCh := make(chan bool, rows*cols)
 	for y := 0; y < rows; y++ {
 		for x := 0; x < cols; x++ {
-			col := make([]float32, colSize)
-			idx2 := 0
-			sy := y * stride
-			sx := x * stride
-			for c := range img {
-				for i := sy; i < sy+kernelSize; i++ {
-					for j := sx; j < sx+kernelSize; j++ {
-						col[idx2] = img[c][i][j]
-						idx2++
+			go func(x, y, idx1 int, doneCh chan bool) {
+				col := make([]float32, colSize)
+				idx2 := 0
+				sy := y * stride
+				sx := x * stride
+				for c := range img {
+					for i := sy; i < sy+kernelSize; i++ {
+						for j := sx; j < sx+kernelSize; j++ {
+							col[idx2] = img[c][i][j]
+							idx2++
+						}
 					}
 				}
-			}
-			res[idx1] = col
+				res[idx1] = col
+				doneCh <- true
+			}(x, y, idx1, doneCh)
 			idx1++
 		}
+	}
+	for i := 0; i < rows*cols; i++ {
+		<-doneCh
 	}
 	return res
 }
