1eb44879d199a8b0ba949617a450b1a56d731e48
Rompei <rompei0924@gmail.com>
2016-08-21 10:49:27 +0900

    Add blas to fullyconnected.

--- layers/fullconnect.go
+++ layers/fullconnect.go
@@ -1,7 +1,7 @@
 package layers
 
 import (
-	"github.com/Rompei/mat"
+	"github.com/gonum/matrix/mat64"
 )
 
 // FullconnectLayer is a layer.
@@ -29,23 +29,22 @@ func NewFullconnectLayer(name, t string, width, height int, biasTerm bool) *Full
 
 // Forward forawards a step.
 func (f *FullconnectLayer) Forward(input [][][]float32) ([][][]float32, error) {
-	in := make([][]float32, f.Width)
+	in := make([]float64, f.Width)
 
 	idx := 0
 	for i := range input {
 		for j := range input[i] {
 			for k := range input[i][j] {
-				in[idx] = []float32{input[i][j][k]}
+				in[idx] = float64(input[i][j][k])
 				idx++
 			}
 		}
 	}
-	inMat := mat.NewMatrix(in)
-	weights := mat.NewMatrix(f.Weights)
-	out, err := mat.Mul(weights, inMat)
-	if err != nil {
-		return nil, err
-	}
+
+	inMat := mat64.NewDense(f.Width, 1, in)
+	weights := ConvertMatrix(f.Weights)
+	var out mat64.Dense
+	out.Mul(weights, inMat)
 
 	output := make([][][]float32, f.Height)
 	for i := range output {
@@ -53,7 +52,7 @@ func (f *FullconnectLayer) Forward(input [][][]float32) ([][][]float32, error) {
 		for j := range output[i] {
 			output[i][j] = make([]float32, 1)
 			for k := range output[i][j] {
-				output[i][j][k] = out.M[i][0] + f.Bias[i]
+				output[i][j][k] = float32(out.At(i, 0)) + f.Bias[i]
 			}
 		}
 	}
